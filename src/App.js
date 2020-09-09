0f83291cc405ee45b391e26956de9a5933339665
Cristian Orozco <cristianorozco@gmail.com>
2020-09-08 21:19:04 -0500

    -grid/list view -styling

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 import Home from './components/Home/Home'
 import Details from './components/Details/Details'
-import './App.css'
+import './App.scss'
 
 function App() {
 	return (
-		<div className='App'>
+		<div className='app'>
 			<Router>
 				<Link to='/'>
-					<h1>Comic Book</h1>
+					<header>
+						<h1>Comic Book</h1>
+					</header>
 				</Link>
 				<Route path='/' exact component={Home} />
 				<Route path='/details/:id' exact component={Details} />
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTh, faBars } from '@fortawesome/free-solid-svg-icons'
 
 import { Format, sortByMostRecent } from '../../utils/Utils'
 import { getComics } from '../../utils/getUrls'
+import './Home.scss'
 
 const List = () => {
 	const [comics, setComics] = useState([])
 	const [loading, setLoading] = useState(true)
 	const [dataError, setDataError] = useState(false)
 	const [messageError, setMessageError] = useState('')
+	const [active, setActive] = useState(true)
 
 	const getData = async () => {
 		const data = await getComics()
@@ -33,23 +37,44 @@ const List = () => {
 	return (
 		<>
 			{dataError === false ? (
-				<div>
-					<h1>Latest Issues</h1>
-					<ul>
+				<>
+					<div className='navbar-space'></div>
+					<div className='navbar'>
+						<h3>Latest Issues</h3>
+						<div className='navbar__icons'>
+							<span
+								className={!active ? 'navbar__icons active' : 'navbar__icons'}
+								onClick={() => setActive(false)}>
+								<FontAwesomeIcon icon={faBars} />
+								<div className='navbar__name'>List</div>
+							</span>
+							<span
+								className={active ? 'navbar__icons active' : 'navbar__icons'}
+								onClick={() => setActive(true)}>
+								<FontAwesomeIcon icon={faTh} />
+								<div className='navbar__name'>Grid</div>
+							</span>
+						</div>
+					</div>
+					<div className='wrap'>
 						{comics.map((comic) => (
-							<li key={comic.id}>
+							<div
+								key={comic.id}
+								className={active ? 'card grid' : 'card list'}>
 								<Link to={`/details/4000-${comic.id}`}>
 									<img src={comic.image.original_url} alt=''></img>
-									<h3>
-										{comic.name ? comic.name : comic.volume.name} #
-										{comic.issue_number}
-									</h3>
-									<p>{Format(comic.date_added)}</p>
+									<div className='lead'>
+										<h3>
+											{comic.name ? comic.name : comic.volume.name} #
+											{comic.issue_number}
+										</h3>
+										<p>{Format(comic.date_added)}</p>
+									</div>
 								</Link>
-							</li>
+							</div>
 						))}
-					</ul>
-				</div>
+					</div>
+				</>
 			) : (
 				<h2>{messageError}</h2>
 			)}
